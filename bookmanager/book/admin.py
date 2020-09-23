4d066bcc18fba3ba5b89a7ae8f60f929832ffaf7
abby <350121189@qq.com>
2020-09-23 13:36:59 +0800

    admin站点

--- bookmanager/book/admin.py
+++ bookmanager/book/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from book.models import BookInfo,PeopleInfo
+# 注册书籍模型
+admin.site.register(BookInfo)
+# 注册人物模型
+admin.site.register(PeopleInfo)
+
