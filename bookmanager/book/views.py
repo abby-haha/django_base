2ab1ef3b046141ba10ee2f697ae7761a4c763c77
abby <350121189@qq.com>
2020-09-23 15:39:07 +0800

    模板

--- bookmanager/book/views.py
+++ bookmanager/book/views.py
@@ -4,5 +4,7 @@ from django.shortcuts import render
 from django.http import HttpRequest
 from django.http import HttpResponse
 def index(request):
+    context = {'name':'双十一啦'}
 
-    return HttpResponse('OK')
+    # return HttpResponse('OK')
+    return render(request,'book/index.html',context=context)
