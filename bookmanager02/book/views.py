c1f153833b52f6a8a341cb2388fb1a8b1d7d3cf0
abby <350121189@qq.com>
2020-09-25 06:57:59 +0800

    其他查询操作

--- bookmanager02/book/views.py
+++ bookmanager02/book/views.py
@@ -42,24 +42,24 @@ BookInfo.objects.filter(id=5).update(name='python入门方法', commentcount=999
 # 删除分2种，物理删除（这条记录的数据 删除） 和 逻辑删除 （修改标记位 例如 is_delete=False)
 
 # 方法1:
-book=BookInfo.objects.get(id=6)
+book = BookInfo.objects.get(id=6)
 book.delete()
 
 # 方式2
 BookInfo.objects.get(id=6).delete()
 BookInfo.objects.filter(id=5).delete()
 
-
 # ##################### 查询 #################################
 # get 查询单一结果，如果不存在会抛出模型类.DoesNotExist异常。
 try:
-    book=BookInfo.objects.get(id=1)
+    book = BookInfo.objects.get(id=1)
 except BookInfo.DoesNotExist:
     print('查询结果不存在')
 
 # all 查询多个结果
-BookInfo.objects.all()
+BookInfo.objePcts.all()
 from book.models import PeopelInfo
+
 PeopelInfo.objects.all()
 
 # count 查询结果数量
@@ -78,9 +78,9 @@ BookInfo.objects.count()
 # 模型类名.objects.get(属性名__运算符=值) 获取1个结果 或者异常
 
 # 查询编号为1的图书
-book=BookInfo.objects.get(id=1) #简写形式（属性名=值）
-book=BookInfo.objects.get(id__exact=1) #完整形式
-BookInfo.objects.get(pk=1) #pk primary key 主键
+book = BookInfo.objects.get(id=1)  # 简写形式（属性名=值）
+book = BookInfo.objects.get(id__exact=1)  # 完整形式
+BookInfo.objects.get(pk=1)  # pk primary key 主键
 BookInfo.objects.exclude(id=1)  # 查询除了id=1的其他书
 BookInfo.objects.filter(id=1)
 
@@ -92,18 +92,91 @@ BookInfo.objects.filter(name__endswith='部')
 
 # 查询书名为空的图书
 BookInfo.objects.filter(name__isnull=True)
-BookInfo.objects.filter(name__isnull=False) # 书名不为空
+BookInfo.objects.filter(name__isnull=False)  # 书名不为空
 
 # 查询编号为1或3或5的图书
-BookInfo.objects.filter(id__in=[1,3,5])
+BookInfo.objects.filter(id__in=[1, 3, 5])
 
 # 查询编号大于3的图书
 BookInfo.objects.filter(id__gt=3)
-BookInfo.objects.filter(id__gte=3) #大于等于3
+BookInfo.objects.filter(id__gte=3)  # 大于等于3
 
 # 查询1980年发表的图书
 BookInfo.objects.filter(pub_date__year=1980)
-BookInfo.objects.filter(pub_date__month=5) # 5月
+BookInfo.objects.filter(pub_date__month=5)  # 5月
 
 # 查询1990年1月1日后发表的图书
 BookInfo.objects.filter(pub_date__gt='1990-1-1')
+
+######################### F 对象 （两个属性的比较查询）#############
+from django.db.models import F
+
+# 使用：2个属性的比较
+# 语法形式：以 filter 为例 模型类名.objects.filter（属性名_运算符=F('第二个属性名'))
+
+# 查询阅读量大于等于评论量的图书
+BookInfo.objects.filter(readcount__gte=F('commentcount'))
+
+############################## 并且查询 ###############
+# 阅读量大于20，并且编号小于3的图书
+BookInfo.objects.filter(readcount__gte=20).filter(id__lt=3)
+# 或者
+BookInfo.objects.filter(readcount__gte=20, id__lt=3)
+
+############################# 或者查询 ###########################
+
+from django.db.models import Q
+
+# 或者语法： 模型类名.objecs.filter(Q（属性名__运算符=值）|Q（属性名__运算符=值）|...)
+# 并且语法： 模型类名.objecs.filter(Q（属性名__运算符=值）&Q（属性名__运算符=值）&...)
+
+# 查询阅读量大于20，或者编号小于3的图书
+BookInfo.objects.filter(Q(readcount__gt=20) | Q(id__lt=3))
+# 并且
+BookInfo.objects.filter(Q(readcount__gt=20) & Q(id__lt=3))
+
+# 查询编号不等于3的书籍
+BookInfo.objects.exclude(id=3)
+BookInfo.objects.filter(~Q(id=3))
+
+############################## 聚合函数 ################################33
+from django.db.models import Sum, Max, Min, Avg, Count
+
+# 模型类名.objects.aggregate(xxx('字段名'))
+# 阅读量求和
+BookInfo.objects.aggregate(Sum('readcount'))
+
+######################### 排序 ###############################
+BookInfo.objects.all().order_by('readcount')
+BookInfo.objects.all().order_by('-readcount')
+
+############################ 两个表的级联操作 ############
+# 查询书籍为1的所有人物信息
+book = BookInfo.objects.get(id=1)
+book.peopelinfo_set.all()
+PeopelInfo.objects.filter(book=1)
+
+# 查询人物为1的书籍信息
+
+person = PeopelInfo.objects.get(id=1)
+person.book.name
+person.book.readcount
+
+########################## 关联过滤查询 #######################
+
+#语法形式
+#查询1的数据， 条件为n
+# 模型名.objects.(关联模型类名小写__字段名__运算符=值）
+
+# 查询图书，要求图书人物为郭靖
+BookInfo.objects.filter(peopelinfo__name__exact='郭靖')
+BookInfo.objects.filter(peopelinfo__name='郭靖')
+
+# 查询图书，要求图书中人物的描述包含‘八’
+BookInfo.objects.filter(peopelinfo__description__contains='八')
+
+# 查询书名为“天龙八部”的所有人物
+PeopelInfo.objects.filter(book__name='天龙八部')
+
+# 查询图书阅读量大于30的所有人物
+PeopelInfo.objects.filter(book__readcount__gt=30)
