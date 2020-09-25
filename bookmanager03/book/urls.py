068a0393bdebfc7e4fa95d082342d13879fb8d40
abby <350121189@qq.com>
2020-09-25 18:07:04 +0800

    HttpRequest对象

--- bookmanager03/book/urls.py
+++ bookmanager03/book/urls.py
@@ -1,9 +1,35 @@
 from django.urls import path
 
-from book.views import index, shop
+from book.views import index, shop,register,json,method
+from book.views import set_cookies,get_cookies
+
+
+############################## 自定义路由转换器 ######################
+from django.urls import converters
+from django.urls.converters import register_converter
+# 自定义路由转换器
+class Mobile:
+    # 判断路由是否符合正则，符合则返回给python的视图
+    regex = '1[3-9]\d{9}'
+
+    def to_python(self, value):
+        return value
+
+    # def to_url(self, value):
+    #     return str(value)
+
+# 注册mobile转换器
+# register_converter(转换器类,'转换器名字')
+register_converter(Mobile,'phone')
 
 urlpatterns = [
 
     path('index/',index),
-    path('<aa>/<bb>/',shop)
+    path('<int:aa>/<phone:bb>/',shop),
+    path('register/',register),
+    path('json/',json),
+    path('method/',method),
+    path('set_cookies/',set_cookies),
+    path('get_cookies/',get_cookies),
+
 ]
--- bookmanager03/book/views.py
+++ bookmanager03/book/views.py
@@ -5,5 +5,54 @@ from django.shortcuts import render
 def index(request):
     return HttpResponse('index')
 
+
+
 def shop(request,aa,bb):
+    post = request.POST
+    print(post)
     return HttpResponse('shop')
+
+def register(request):
+    # data = request.POST
+    # print(data)
+    # < QueryDict: {'username': ['itcast'], 'password': ['123']} >
+    return HttpResponse('ok')
+
+def json(request):
+    # body = request.body
+    # print(body)
+    # b'{\n\t"name":"itcast",\n\t"password":"123"\n}'
+    # body_str=body.decode()
+    # print(body_str)
+    # {
+    #     "name": "itcast",
+    #     "password": "123"
+    # }
+    import json
+    # body_dict =json.loads(body_str)
+    # print(body_dict)
+    # {'name': 'itcast', 'password': '123'}
+    print(request.META)
+    return HttpResponse('json')
+
+def method(request):
+    # print(request.method)
+    # POST
+    # GET
+    return HttpResponse('method')
+
+
+
+
+############################ cookies #############################
+def set_cookies(request):
+
+    response= HttpResponse('set_cookies')
+    response.set_cookie('name','username')
+
+    return response
+
+def get_cookies(request):
+    request = request.COOKIES.get('name')
+    print(request)
+    return HttpResponse('get_cookies')
