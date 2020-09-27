1c5fa0e1e3ee8969f8b582745c21b96eb15705b2
abby <350121189@qq.com>
2020-09-28 07:55:17 +0800

    模型遷移(建表)

--- bookmanager04/book/models.py
+++ bookmanager04/book/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+# 書籍列表信息模型
+class BookInfo(models.Model):
+    # id 系統已經自動定義好
+    name = models.CharField(max_length=10)
+
+    class Meta:
+        db_table='bookinfo'
+        verbose_name='書籍管理'
+
+# 人物列表信息模型
+class PeopleInfo(models.Model):
+    name= models.CharField(max_length=10)
+    gender=models.BooleanField()
+    book=models.ForeignKey(BookInfo,on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'peopleinfo'
+        verbose_name = '人物管理'
